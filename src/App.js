fadd5f6864ebabff4fd069337082910ec4ff1768
Chamith <udayangaac@gmail.com>
2021-03-02 10:46:32 +0530

    Theme font changed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import React from "react";
 import DefaultLayout from "./components/layouts/defaultLayout/DefaultLayout";
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import {orange} from '@material-ui/core/colors';
+
 
 function App() {
+
+    const theme = createMuiTheme({
+        status: {
+            danger: orange[500],
+        },
+        typography: {
+            "fontFamily": `'Nunito Sans', sans-serif;`,
+            "fontSize": 14,
+            "fontWeightLight": 300,
+            "fontWeightRegular": 400,
+            "fontWeightMedium": 500
+        }
+    });
+
+
     return (
+        <ThemeProvider theme={theme}>
         <DefaultLayout/>
+        </ThemeProvider>
     );
 }
 export default App;
